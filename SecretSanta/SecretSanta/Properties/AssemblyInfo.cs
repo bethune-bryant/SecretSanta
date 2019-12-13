ca13c6a6222b0c7eadb629ae2106ecda4a02c12d
bethune-bryant <bryantstutoringservice@gmail.com>
2019-12-13 14:47:53 -0600

    Adding version info and making release.

--- SecretSanta/SecretSanta/Properties/AssemblyInfo.cs
+++ SecretSanta/SecretSanta/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SecretSanta")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A program to do secret santa drawings via email.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Bryant Nelson")]
 [assembly: AssemblyProduct("SecretSanta")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2016")]
+[assembly: AssemblyCopyright("Copyright © Bryant Nelson 2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
